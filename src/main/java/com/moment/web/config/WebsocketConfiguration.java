5f1bff70603ac70842e8676cb36fa42a712fafbd
chae401 <chae401@sju.ac.kr>
2023-11-09 16:48:33 +0900

    feat(WebSocketConfiguration): set WebSocket message handling

--- src/main/java/com/moment/web/config/WebsocketConfiguration.java
+++ src/main/java/com/moment/web/config/WebsocketConfiguration.java
@@ -1,4 +1,25 @@
 package com.moment.web.config;
 
-public class WebsocketConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/aed-status")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
 }
