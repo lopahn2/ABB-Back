bbecb7e36986b26779ab4e311f6e5d0c204d0324
chae401 <chae401@sju.ac.kr>
2023-11-09 17:11:47 +0900

    feat(ProducerService): send msg

--- src/main/java/com/moment/web/service/ProducerService.java
+++ src/main/java/com/moment/web/service/ProducerService.java
@@ -1,2 +1,28 @@
-package com.moment.web.service;public class ProducerService {
+package com.moment.web.service;
+
+import com.moment.web.AedDTO;
+import com.moment.web.config.KafkaProperties;
+import java.util.concurrent.CompletableFuture;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class ProducerService {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public String sendMsg(AedDTO aedDTO) {
+        CompletableFuture<SendResult<String, Object>> completableFuture = kafkaTemplate.send(KafkaProperties.TOPIC, aedDTO);
+        completableFuture.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("Send mesage : {}", aedDTO.getId());
+            } else {
+                log.info("Error Occured");
+            }
+        });
+        return "SUCCESS";
+    }
 }
